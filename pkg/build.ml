7c3f63fbc395c962796f12f23f904b9a4f015ce7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-06-10 16:07:46 +0200

    update META

--- pkg/build.ml
+++ pkg/build.ml
@@ -11,7 +11,8 @@ let () =
     ; Pkg.lib ~exts:Exts.library "src/printbox"
     ; Pkg.lib ~exts:Exts.interface "src/PrintBox"
     ; Pkg.lib ~exts:Exts.interface "src/PrintBox_text"
-    ; Pkg.lib ~exts:Exts.module_library ~cond:html "src/html/PrintBox_html"
+    ; Pkg.lib ~exts:Exts.library ~cond:html "src/html/printbox_html"
+    ; Pkg.lib ~exts:Exts.interface "src/html/PrintBox_html"
     ; Pkg.doc "README.md"
     ]
 
