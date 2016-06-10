f2c462ab387ac67ffd5e024579205bdb6a5d5c44
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-06-10 17:31:51 +0200

    fold by default

--- src/html/PrintBox_html.ml
+++ src/html/PrintBox_html.ml
@@ -62,9 +62,13 @@ let to_string b =
 let to_string_doc b =
   let meta_str = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" in
   let footer_str =
-    "<script>[...document.querySelectorAll('p')].forEach(el => \
-     el.addEventListener('click', () => el.nextSibling.style.display = \
-     el.nextSibling.style.display === 'none' ? 'block' : 'none')) </script>"
+    "<script> \
+     [...document.querySelectorAll('p')].forEach(el => \
+       el.addEventListener('click', () => el.nextSibling.style.display = \
+       el.nextSibling.style.display === 'none' ? 'block' : 'none')); \
+     [...document.querySelectorAll('ul ul')].forEach(el => \
+       el.style.display = 'none'); \
+     </script>"
   in
   Format.asprintf "<head>%s%s</head><body>@[%a@]%s</body>@."
     meta_str prelude_str (H.pp_elt ()) (to_html b) footer_str
