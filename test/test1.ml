aea576fe15365161290a25599f415d92571ac256
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-11-12 17:12:42 -0600

    test: add some tests for unicode and align-right

--- test/test1.ml
+++ test/test1.ml
@@ -33,4 +33,44 @@ let grid =
     ]
 
 let () =
-  PrintBox_text.output stdout grid
+  PrintBox_text.output stdout grid;
+  print_endline ""
+
+let grid2 =
+  B.frame @@ B.record ~pad:B.align_right ["name1", B.int 1; "foo", B.bool true]
+
+let () =
+  PrintBox_text.output stdout grid2; print_endline ""
+
+module Box_in = struct
+  let b =
+    let open B in
+    frame @@ grid_l [
+      [ text "a"; text "looooooooooooooooooooooooo\noonng"];
+      [ text "bx"; align_right @@ frame @@ record ["x", int 1; "y", int 2]]
+    ]
+
+  let () = print_endline @@ PrintBox_text.to_string b
+end
+
+module Unicode = struct
+  let string_len s i len =
+    Uutf.String.fold_utf_8 ~pos:i ~len
+      (fun n _ c -> match c with
+        | `Malformed _ -> 0
+        | `Uchar c -> n+ max 0 (Uucp.Break.tty_width_hint c))
+      0 s
+
+  let () = PrintBox_text.set_string_len string_len
+
+  let b =
+    B.(frame @@ vlist [text "nice unicode! 💪"; frame @@
+    hlist [
+      vlist[text "oï ωεird nums:\nπ/2\nτ/4";
+        pad @@ pad @@ tree (text "0")[text "1"; tree (text "ω") [text "ω²"]]];
+      frame @@ frame @@ frame
+      @@ vlist [text "sum=Σ_i a·xᵢ²\n—————\n1+1"; align_right @@ text "Ōₒ\nÀ"]]]);;
+
+  let () = print_endline @@ PrintBox_text.to_string b
+end
+
