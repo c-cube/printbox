3a317c371716a1e1221f2f7801f2c4b7da9438bd
Lukasz Stafiniak <lukstafi@gmail.com>
2023-12-22 12:42:48 +0100

    Test html folding

--- test/test_html.ml
+++ test/test_html.ml
@@ -7,9 +7,10 @@ let b =
       tree (frame @@ text "header 3") [frame @@ text "subchild 3"]
     ];
     tree empty [
-      tree (frame @@ text "header 4") [frame @@ text "subchild 4"]
+      tree (frame @@ text "header 4") [text "subchild 4"]
     ];
+    frame @@ tree (text "header 5") [text "subchild 5"];
     frame @@ text "child 5"
   ]
 
-let () = print_endline @@ PrintBox_html.to_string b
+let () = print_endline @@ PrintBox_html.(to_string ~config:Config.(tree_summary true default)) b
