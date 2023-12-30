43f18f65f7edf6b6af1212674d4cb4abbb107db4
Lukasz Stafiniak <lukstafi@gmail.com>
2023-12-30 21:36:06 +0100

    Fixes #28: no misleading uptick for empty tree nodes

--- test/test_blending.ml
+++ test/test_blending.ml
@@ -9,7 +9,10 @@ let b =
     tree empty [
       tree (frame @@ text "header 4") [frame @@ text "subchild 4"]
     ];
-    frame @@ text "child 5"
+    frame @@ text "child 5";
+    frame @@ tree (frame @@ text "header 6") [
+      tree (frame @@ text "child 6") [frame @@ text "subchild 6"]
+    ]
   ]
 
 let () = print_endline @@ PrintBox_text.to_string b
