43a98f7d583798a8c0c09e94f59a91104cfbf380
Lukasz Stafiniak <lukstafi@gmail.com>
2024-03-01 22:50:10 +0100

    Support `hlist` and `vlist` inside summaries Implemented as poor-man's support of arbitrary grids.

--- test/test_html.ml
+++ test/test_html.ml
@@ -11,6 +11,16 @@ let b =
       tree empty [ tree (frame @@ text "header 4") [ text "subchild 4" ] ];
       frame @@ tree (text "header 5") [ text "subchild 5" ];
       frame @@ text "child 5";
+      text "separator";
+      tree (hlist ~bars:false [text "entry 0.1"; text "entry 0.2"]) [text "child 5.5"];
+      text "separator";
+      tree (hlist ~bars:false [text "entry 1"; frame @@ text "entry 2"]) [text "child 6"];
+      text "separator";
+      tree (hlist ~bars:true [text "entry 3"; frame @@ text "entry 4"]) [text "child 7"];
+      text "separator";
+      tree (vlist ~bars:false [text "entry 5"; frame @@ text "entry 6"]) [text "child 8"];
+      text "separator";
+      tree (vlist ~bars:true [text "entry 7"; frame @@ text "entry 8"]) [text "child 9"];
     ]
 
 let () =
