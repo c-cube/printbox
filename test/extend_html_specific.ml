1692771f79f24b8b333e73f491dee1a7c2d1bc7d
Lukasz Stafiniak <lukstafi@gmail.com>
2024-09-30 16:17:13 +0200

    Standalone test for extending `printbox-html`, its dependency on `printbox-text` is spurious

--- test/extend_html_specific.ml
+++ test/extend_html_specific.ml
@@ -6,13 +6,6 @@ module H = Html
 
 type B.ext += Hello_html of string | Hello_with_header of string
 
-let text_handler ext =
-  match ext with
-  | Hello_html txt -> PrintBox_text.to_string B.(frame @@ text txt)
-  | Hello_with_header txt ->
-    PrintBox_text.to_string B.(tree (text "Greetings!") [ frame @@ text txt ])
-  | _ -> invalid_arg "text_handler: unrecognized extension"
-
 let html_handler config ext =
   match ext with
   | Hello_html txt -> (H.button [ H.txt txt ] :> PrintBox_html.toplevel_html)
@@ -26,7 +19,6 @@ let html_handler config ext =
   | _ -> invalid_arg "html_handler: unknown extension"
 
 let () =
-  PrintBox_text.register_extension ~key:"Hello_world" text_handler;
   PrintBox_html.register_extension ~key:"Hello_world" html_handler
 
 let test1 = B.extension ~key:"Hello_world" @@ Hello_html "Hello world!"
@@ -35,12 +27,8 @@ let test2 =
   B.extension ~key:"Hello_world" @@ Hello_with_header "Hello wide world!"
 
 let () =
-  print_endline "Text output simple:";
-  print_endline @@ PrintBox_text.to_string test1;
   print_endline "\nHTML output simple:";
   print_endline @@ PrintBox_html.to_string test1;
-  print_endline "\nText output with header:";
-  print_endline @@ PrintBox_text.to_string test2;
   print_endline "\nHTML output with header:";
   print_endline
   @@ PrintBox_html.(to_string ~config:Config.(tree_summary true default) test2)
